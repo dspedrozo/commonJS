dfd3edba9da4e8dae02c81216cd70975814d5f54
Dani <danielapedrozo@gmail.com>
2022-07-27 22:01:37 -0300

    Ejemplo de commonjs

---
+++ Index.js
@@ -0,0 +1,11 @@
+const operaciones = require("./Operaciones.cjs")
+
+
+
+console.log("Suma: " + operaciones.addition(8,2));
+
+console.log("Resta: " + operaciones.subtraction(8,2));
+
+console.log("División: " + operaciones.division(8,2));
+
+console.log("Multiplicación: " + operaciones.multiplication(8,2));
---
+++ Operaciones.cjs
@@ -0,0 +1,9 @@
+
+
+exports.addition = (X, Y) => X + Y;
+
+exports.subtraction = (X,Y) => X-Y;
+
+exports.multiplication = (X,Y) => X*Y;
+
+exports.division = (X,Y) => X/Y;
